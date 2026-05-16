3c1e78803389a61bc4ff6d4dc6b26a6ecaa386bd
NakshatraDigala <sainakshatra2007@gmail.com>
2026-05-16 19:50:10 +0530

    Add files via upload

---
+++ may16(Loops).py
@@ -0,0 +1,95 @@
+#Examples
+'''
+a=[10,20,30,40,50]
+for i in a:
+    print(i)#returns values in a
+#if needed in single line:
+for i in a:
+    print(i,end=' ')
+'''
+'''
+a=[10,20,30,40,50]
+for i in a:
+    print(a)#prints list i times
+'''
+#type of a and elements in a
+'''
+a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+'''
+'''
+a=(10,20,30,40,50)
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+'''
+'''
+a={10,20,30,40,50}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+'''
+a={'year':2026,'month':'may','date':16}
+'''
+for i in a:
+    print(i)#returns keys only
+    print(type(a))
+    print(type(i))
+'''
+'''
+for i in a.keys():
+    print(i)
+    print(type(a))
+    print(type(i))
+'''
+'''
+for i in a.values():#returns only values
+    print(i)
+    print(type(a))
+    print(type(i))
+'''
+'''
+for i in a.items():#returns both keys and values
+    print(i)
+    print(type(a))
+    print(type(i))
+'''
+'''
+a='codegnan'
+for i in a:
+    print(i,end=' ')
+'''
+'''
+a=[4.5,5.6]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+'''
+'''
+a=[2+8j,3+4j,5+6j]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+'''
+'''
+a=[True,False,True]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+'''
+#TASK
+'''
+a=['code','codegnan','python']
+b=[]
+for i in a:
+    b.append(i.upper()+' ')
+print(b)
+'''
---
+++ may16(tasks).py
@@ -0,0 +1,45 @@
+#SOCIAL MEDIA LOGIN(nested if)
+'''
+a=input('Enter username: ')
+if a=='nakshatra':       #if a=='nakshatra' and b=='naks123':(this is another way too)
+    b=input('Password: ')
+    if b=='naks123':
+        print('Login successful')
+    else:
+        print('Password incorrect')
+else:
+    print('Invalid username')
+'''
+
+#BAKERY(if-elif-else)
+'''
+a=input('Enter cake name: ')
+if a=='Red velvet':
+    print('1000/-')
+elif a=='Choco Almond':
+    print('800/-')
+elif a=='Butterscotch':
+    print('600-')
+elif a=='Chocolate':
+    print('400/-')
+else:
+    print('Sorry cake is not available')
+'''
+
+#PIZZA(if-elif-else)
+'''
+a=int(input('Enter price: '))
+if a==1000:
+      print('BBQ Pizza')
+elif a==800:
+    print('Crispy Chicken pizza')
+elif a==600:
+    print('Panner pizza')
+elif a==400:
+    print('Corn pizza')
+elif a==200:
+    print('French fries and coke')
+'''
+
+
+#--------------------------CONDITIONS CONCEPT ENDED------------------------------
